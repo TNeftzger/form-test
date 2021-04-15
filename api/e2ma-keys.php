ce6c4aeae3d6820989444b43eeb98a499757552c
Tyra Neftzger <41329142+TNeftzger@users.noreply.github.com>
2021-04-15 13:31:30 -0500

    Form Not Updating
    The form is working correctly. Via Postman my first and last name were updated correctly.

---
+++ api/e2ma-keys.php
@@ -0,0 +1,11 @@
+<?php
+//URL Prefix for testing
+$urlPrefix = "https://api.e2ma.net/";
+//Emma Account ID
+$account_id = "1931275";
+//Emma API Key or Manager Username
+$public_api_key = "17dcaef69ffd22be4398";
+//Emma API Secret or Manager Password
+$private_api_key = "caf2024b152ce7b6a17a";
+//Signup Form ID
+$form_id = "1943656";
