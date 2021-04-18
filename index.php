3a1f151020e5dbc12a9b736d3757a3605dd52b05
Tyra Neftzger <41329142+TNeftzger@users.noreply.github.com>
2021-04-18 09:41:00 -0500

    New Singup Form
    I built the form in Dreamweaver and then included the code within the index.php and added my CSS style sheet.

--- index.php
+++ index.php
@@ -7,15 +7,38 @@ require 'api/e2ma-keys.php';
 
 <!DOCTYPE html>
 
-  <?php require 'blocks/head.php'; ?>
-  <body>
-    <main>
-      <?php require 'blocks/form.php'; ?>
-    </main>
-    <!-- end #content -->
+  <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Basic Sign Up Form</title>
+        <link rel="stylesheet" href="css/styles.css">
+      </head>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script> 
-    <script src="js/e2ma.js"></script>
+  <body>
+    <form action="index.html" method="post">
+
+      <h1>Sign Up</h1>
+
+      <fieldset>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="user_name" value="Name">
+
+        <label for="mail">Email:</label>
+        <input type="email" id="mail" name="user_email" value="Email">
+
+        <label for="date">Birthday:</label>
+        <input type="date" id="date" name="user_bday" value="Birthday">
+
+        <label>Subsribe me to:</label>
+        <input type="checkbox" id="development" value="interest_development" name="user_interest"><label class="soft" for="development">Coupons</label><br>
+
+        <input type="checkbox" id="design" value="interest_design" name="user_interest"><label class="soft" for="design">Weekly Newsletter</label><br>
+
+        <input type="checkbox" id="business" value="interest_business" name="user_interest"><label class="soft" for="business">Daily Updates</label>
+      </fieldset>
+
+      <button type="submit">Sign Up</button>
+    </form>
 
   </body>
 </html>
